d9d669c57c7b24d05ad0f51bab064ebc3ba5618c
OnurKaraoz <OnurKaraoz@NIRVANASERVER>
2022-04-11 16:10:17 +0300

    Add project files.

---
+++ CSharp-Ornek1-YuzdeHesaplama/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Ornek1_YuzdeHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Yuzde Hesaplama
+            // A sayısının yüzde B'si kaçtır?
+
+            double numbers1, numbers2,result1,result2;
+            Console.WriteLine("Lütfen Sayı giriniz (A SAYISI) :");
+            numbers1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Lütfen Yüzde giriniz (B SAYISI) :");
+            numbers2 = Convert.ToDouble(Console.ReadLine());
+            result1 = numbers1 * numbers2 / 100;
+            Console.WriteLine("Girilen Sayının % :" + result1);
+            result2 = numbers1 - result1;
+            Console.WriteLine("Kalan : " + result2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Form-1-YuzdeHesaplama/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_1_YuzdeHesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double numbers1 =Convert.ToDouble(textBox1.Text);
+            double numbers2 = Convert.ToDouble(textBox2.Text);
+            double result= numbers1 * numbers2 / 100;
+            double result2 = numbers1 - result ;
+            textBox3.Text = result.ToString();
+            textBox4.Text = result2.ToString();
+        }
+    }
+}
