576859d3ca418a060b1c092328ce432d611241e9
OnurKaraoz <OnurKaraoz@NIRVANASERVER>
2022-04-11 16:28:18 +0300

    cscs

---
+++ Console-2-KarHesaplama/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_2_KarHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //KarHesaplama
+            double numbers, numbers2, numbers3;
+            Console.WriteLine("Sayı girini");
+            numbers =Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Kar girini");
+            numbers2 = Convert.ToDouble(Console.ReadLine());
+            numbers3 = numbers2 + (numbers* numbers2/100);
+
+            Console.WriteLine("Sonuç :"+ numbers3);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Form-2-KarHesaplama/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_2_KarHesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double numbers1=Convert.ToDouble(textBox1.Text);
+            double numbers2=Convert.ToDouble(textBox2.Text);
+            double numbers3 = numbers2 + (numbers1* numbers2 / 100);
+            textBox3.Text = numbers3.ToString();
+        }
+    }
+}
